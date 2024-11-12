eef629976b8dde5859f1e2f23e87bcfe7ac6fc0c
vaibhav-chouksey <vaibhavchouksey07@gmail.com>
2024-11-12 18:59:18 +0530

    initial commit

---
+++ prime.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter the number"<<endl;
+    cin>>a;
+    for(int i=2;i<a;i++){
+        if(a%i!=0){
+            cout<<"Prime no.";
+            break;
+        }
+        else
+        cout<<"not a prime number";
+        break;
+
+    }
+
+}
